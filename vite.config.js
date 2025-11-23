2e36d09801c3b5e6a84b8f44d27f2998d46ad1dc
Ashish <sunitasharma8800366473@gmail.com>
2025-11-23 14:52:50 +0530

    I am dumb

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: processe.env.VITE_BASE_PATH || "/ASInteriorWork",
+  base: process.env.VITE_BASE_PATH || "/ASInteriorWork",
 })
