caace3fc94f0dcad19a6b58f2c03f64501620998
Ashish <sunitasharma8800366473@gmail.com>
2025-11-23 15:48:50 +0530

    new

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -9,7 +9,7 @@ function End() {
 
       <div className=" h-full w-full flex flex-col md:justify-center justify-start md:pt-0 pt-5 md:items-end items-start  gap-y-2  text-[#b6b6b7] md:pl-0 pl-5 text-[1.1rem] md:pr-[8rem]">
         <h1 className=''>ssanilsharmass471@gmail.com</h1>
-        <h1>+91-8800366473</h1>
+        <h1>+91-8800366471</h1>
       </div>
     </div>
   )
